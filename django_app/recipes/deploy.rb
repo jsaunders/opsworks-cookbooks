388c700eb63796b97ae2ba50cdbb6641aabc6644
Alex Brinsmead <alexbrinsmead@aviamedia.com>
2014-09-28 16:27:50 -0700

    test git clone

--- django_app/recipes/deploy.rb
+++ django_app/recipes/deploy.rb
@@ -1,11 +1,5 @@
 node[:deploy].each do |application, deploy|
 
-    clone_repo = application['scm']['repository']["https://github.com/"]=""
-
-    file "/home/ubuntu/clone_repo.log" do
-        content clone_repo
-    end
-
     file "/home/ubuntu/.ssh/id_rsa" do
         content application['scm']['ssh_key']
     end
