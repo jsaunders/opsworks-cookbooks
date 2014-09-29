7804530cbe32f089f0bca1c4344c573b8c23af61
Alex Brinsmead <alexbrinsmead@aviamedia.com>
2014-09-28 17:26:56 -0700

    test git clone

--- django_app/recipes/deploy.rb
+++ django_app/recipes/deploy.rb
@@ -1,16 +1,5 @@
 include_recipe 'deploy'
 
-node[:deploy].each do |application, deploy|
-
-    file "/home/ubuntu/.ssh/id_rsa" do
-        content node[application]['scm']['ssh_key']
-    end
-
-    execute "chmod deploy key" do
-        command "chmod 600 /home/ubuntu/.ssh/id_rsa"
-    end
-
-end
 
 
 
