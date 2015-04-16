3700b2f18809161f81458f4edb554a5d8530f5aa
Alex Brinsmead <alexbrinsmead@aviamedia.com>
2015-04-15 22:26:39 -0700

    install celery prior to deploy

--- django_app/recipes/setup.rb
+++ django_app/recipes/setup.rb
@@ -4,18 +4,18 @@ script "install_dependencies" do
   cwd "/tmp"
   code <<-EOH
 
+    sudo apt-get update
     apt-get install -y git
     apt-get install -y apache2
     apt-get install -y libapache2-mod-wsgi
-
-    # postgresql dependencies
-    #apt-get install -y python-dev
-    #apt-get install -y libpq-dev
     apt-get install -y python-psycopg2
 
     # ubuntu pip package is currently broken
     # https://bugs.launchpad.net/ubuntu/+source/python-pip/+bug/1306991
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+
+    # install celery
+    pip install celery==3.1.17
 
     # disable default apache site
     a2dissite 000-default
