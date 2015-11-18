adf4d0d521431590c7dd00ef578a4273d793d71b
Jordan Saunders <jordan@jsaundersdev.com>
2015-11-18 13:59:47 -0500

    nginx cookbook initial commmit

---
+++ nginx/recipes/setup.rb
@@ -0,0 +1,20 @@
+package "nginx" do
+  :upgrade
+end
+
+service "nginx" do
+  enabled true
+  running true
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+end
+
+
+cookbook_file "/etc/nginx/nginx.conf" do
+  source "nginx-app.conf"
+  mode 0640
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "nginx")
+
+end
