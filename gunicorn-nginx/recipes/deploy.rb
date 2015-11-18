6e7dfd814bc88c018d300847c3758911797f7c98
Jordan Saunders <jordan@jsaundersdev.com>
2015-11-18 12:46:35 -0500

    gunicorn cookbook

---
+++ gunicorn-nginx/recipes/deploy.rb
@@ -0,0 +1,89 @@
+include_recipe 'deploy'
+
+
+project_path = node[:django_app][:project_path]
+project_name = node[:django_app][:project_name]
+requirements_path = node[:django_app][:requirements_path]
+htpasswd = node[:django_app][:htpasswd]
+force_https = node[:django_app][:force_https]
+
+node[:deploy].each do |application, deploy|
+
+    opsworks_deploy do
+      app application
+      deploy_data deploy
+    end
+
+    template '/etc/apache2/sites-available/site.conf' do
+        source "site.erb"
+        owner 'root'
+        group 'root'
+        mode '600'
+        variables({
+            :project_path => project_path,
+            :project_name => project_name,
+            :environment => deploy[:environment_variables],
+            :htpasswd  => htpasswd,
+            :force_https => force_https
+        })
+    end
+
+    template '/etc/init.d/celeryd' do
+        source 'celeryd_init.erb'
+        owner 'root'
+        group 'root'
+        mode '755'
+    end
+
+    template '/etc/default/celeryd' do
+        source 'celeryd_conf.erb'
+        owner 'root'
+        group 'root'
+        mode '600'
+        variables({
+            :project_path => project_path,
+            :project_name => project_name,
+            :environment => deploy[:environment_variables]
+        })
+    end
+
+    template '/etc/apache2/.htpasswd' do
+        source 'htpasswd.erb'
+        owner 'root'
+        group 'root'
+        mode '644'
+        variables({
+            :htpasswd  => htpasswd
+        })
+    end
+
+    # Export env variables so migrate can access DJANGO_SETTINGS_FILE
+    deploy[:environment_variables].each do |key, value|
+        ENV[key] = value
+    end
+
+end
+
+
+script "install dependencies and activate" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+
+    # install requirements from text file
+    pip install -r "#{requirements_path}"
+
+    # run migrations
+    python #{project_path}/manage.py migrate
+
+    a2ensite site
+    a2enmod rewrite
+    service apache2 reload
+    service apache2 start
+
+    # start or restart celery
+    sudo service celeryd restart
+
+    EOH
+
+end
---
+++ gunicorn-nginx/recipes/setup.rb
@@ -0,0 +1,42 @@
+script "install_dependencies" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+
+    sudo apt-get update
+    apt-get install -y python python-dev python-setuptools python-software-properties vim
+    apt-get install -y libpq-dev
+    apt-get install -y supervisor
+    apt-get install -y git
+
+    # add nginx stable ppa
+    add-apt-repository -y ppa:nginx/stable
+    # update packages after adding nginx repository
+    apt-get update
+    # install latest stable nginx
+    apt-get install -y nginx
+    apt-get install -y rabbitmq-server
+
+    # install pip
+    easy_install pip
+
+    # install uwsgi now because it takes a little while
+    pip install uwsgi
+
+    un useradd celeryuser
+    apt-get install -y python-psycopg2
+
+    # ubuntu pip package is currently broken
+    # https://bugs.launchpad.net/ubuntu/+source/python-pip/+bug/1306991
+    # curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+
+    # install celery
+    pip install celery==3.1.17
+
+    # disable default apache site
+    # a2dissite 000-default
+
+  EOH
+
+end
