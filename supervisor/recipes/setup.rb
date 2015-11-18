ec694825067e958ee00440ce99e5bc520a6ca99f
Jordan Saunders <jordan@jsaundersdev.com>
2015-11-18 18:24:47 -0500

    supervisor service

--- supervisor/recipes/setup.rb
+++ supervisor/recipes/setup.rb
@@ -1,1 +1,9 @@
 include_recipe 'supervisor'
+
+
+supervisor_service "app-uwsgi" do
+  action :enable
+  autostart false
+  command '/usr/local/bin/uwsgi --ini /home/ubuntu/bespoke/current/uwsgi.ini'
+
+end
